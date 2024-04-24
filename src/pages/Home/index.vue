0ace7d8b20c5b99df10f08fcdcd3dd75536fe238
cycy <1531859775@qq.com>
2024-04-24 11:45:13 +0800

    feat: home页其他组件的静态拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
-    <div>主页</div>
+    <!-- home主页 -->
     <!-- 三级联动全局组件已注册为全局组件，不需要引入 -->
     <TypeNav />
+    <!-- 其余组件 -->
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
+// 引入其余非全局组件
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
   name: "home",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
 };
 </script>
 
